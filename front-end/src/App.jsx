8dcfc54ceadd60bcac34cc0075e79bcfa0448746
ikramKhan <ikram@gro-technologies.com>
2024-02-16 23:49:40 +0500

    Routes and pages created

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,25 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
-    <main className="text-3xl">
-      Blog
-    </main>
-  )
+    <Routes>
+      <React.Fragment>
+        <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+      </React.Fragment>
+    </Routes>
+  );
 }
 
 export default App
