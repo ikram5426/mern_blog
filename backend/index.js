bbb908768db2eb4c4755779a89ea6bbdf471108d
ikramKhan <ikram@gro-technologies.com>
2024-02-17 18:18:33 +0500

    Database configration done using mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
-app.listen(5000, () => {
-  console.log('app listening on port 5000');
+// MongoDB uri 
+const mongo_uri = process.env.MONGO_URI
+
+//Port
+const port = process.env.PORT
+
+// MongoDB connection
+mongoose.connect(mongo_uri).then(() => { console.log(`data base connected successfully on port ${port}`); })
+
+// Server configuration
+app.listen(port, () => {
+  console.log('Server started successfully');
 })
